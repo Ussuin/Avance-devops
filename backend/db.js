5e5b419116b63d5d96df12bff6ad4e8b22b10c90
Ussuin <emanuelgzzxd@gmail.com>
2026-04-16 22:55:43 -0600

    actualizando el db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'localhost',   
+  host: process.env.DB_HOST || '127.0.0.1',   
   user: process.env.DB_USER || 'root',        
   password: process.env.DB_PASSWORD || '123', 
   database: process.env.DB_NAME || 'cryptodb' 
